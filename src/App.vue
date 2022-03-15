8c02780a05aec1354f824b96079092b4eb137069
wxQTFY <wx392817271@163.com>
2022-03-15 15:47:09 +0800

    vue_demo

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+
+<template>
+    <el-container>
+      <el-header>模拟彩票自选系统</el-header>
+      <el-container>
+        <el-aside width="200px">
+          <TicAside></TicAside>
+        </el-aside>
+        <el-container>
+          <el-main>Main</el-main>
+        </el-container>
+      </el-container>
+      <el-footer>Design ©2022 Created by wang</el-footer>
+    </el-container>
+</template>
+
+<script>
+import TicAside from './components/TicAside.vue'
+ export default{
+   components:{
+     TicAside
+   }
+ }
+
+</script>
+
+<style>
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+</style>
---
+++ src/components/TicAside.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-row class="tac">
+    <el-col>
+      <el-menu
+        default-active="1"
+        class="el-menu-vertical-demo"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+        <el-menu-item index="1" v-for="tic in tictype" :key="id">
+          <i class="el-icon-menu"></i>
+          <span slot="title">{{tic.ticname}}</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+  export default{
+    data(){
+      return{
+        tictype:[
+          {id:1, ticname:'双色球'},
+          {id:2, ticname:'大乐透'},
+          {id:3, ticname:'快乐8'}
+        ]
+      }
+    }
+  }
+
+</script>
+
+<style>
+</style>
